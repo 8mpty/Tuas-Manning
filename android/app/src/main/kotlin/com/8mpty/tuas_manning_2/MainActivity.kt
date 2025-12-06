2bd18b8b01afe45fc117c4f7165e608497354ecb
Khai <contact@8mpty.com>
2025-12-06 20:24:20 +0800

    1

--- android/app/src/main/kotlin/com/8mpty/tuas_manning_2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.8mpty.tuas_manning_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Running [flutter clean] & [flutter pub get]"
-flutter clean
-flutter pub get
-echo "Flutter setup completed!"
